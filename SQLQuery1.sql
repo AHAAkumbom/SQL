c5ce3940a7af1633645435ca3acd163b3f285d18
HaggaiAkumbom <44718084+AHAAkumbom@users.noreply.github.com>
2023-01-05 05:21:59 +0100

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,68 @@
+--Create Table EmployeeDemographics 
+--(EmployeeID int, 
+--FirstName varchar(50), 
+--LastName varchar(50), 
+--Age int, 
+--Gender varchar(50)
+--)
+
+
+--Create Table EmployeeSalary 
+--(EmployeeID int, 
+--JobTitle varchar(50), 
+--Salary int
+--)
+
+--Insert into EmployeeDemographics VALUES
+--(1001, 'Jim', 'Halpert', 30, 'Male'),
+--(1002, 'Pam', 'Beasley', 30, 'Female'),
+--(1003, 'Dwight', 'Schrute', 29, 'Male'),
+--(1004, 'Angela', 'Martin', 31, 'Female'),
+--(1005, 'Toby', 'Flenderson', 32, 'Male'),
+--(1006, 'Michael', 'Scott', 35, 'Male'),
+--(1007, 'Meredith', 'Palmer', 32, 'Female'),
+--(1008, 'Stanley', 'Hudson', 38, 'Male'),
+--(1009, 'Kevin', 'Malone', 31, 'Male')
+
+--Insert Into EmployeeSalary VALUES
+--(1001, 'Salesman', 45000),
+--(1002, 'Receptionist', 36000),
+--(1003, 'Salesman', 63000),
+--(1004, 'Accountant', 47000),
+--(1005, 'HR', 50000),
+--(1006, 'Regional Manager', 65000),
+--(1007, 'Supplier Relations', 41000),
+--(1008, 'Salesman', 48000),
+--(1009, 'Accountant', 42000)
+
+
+/*
+Select Statment
+*,Top,Distinct,Count,As,Max,Min,Avg
+*/
+
+--SELECT *
+--FROM SQLProject.dbo.EmployeeSalary
+
+/*
+Where Statment
+=,<>,<,>,And,Or,Like,Null,Not Null,In
+*/
+
+--SELECT *
+--FROM SQLProject.dbo.EmployeeDemographics
+--WHERE FirstName IN ('Jim','Michael')
+
+/*
+Group By, Order By
+*/
+SELECT*
+FROM  EmployeeDemographics
+ORDER BY Age DESC , Gender ASC
+
+
+--SELECT Gender ,COUNT(Gender) AS CountGender
+--FROM EmployeeDemographics
+--WHERE Age >31
+--GROUP BY Gender
+--ORDER BY Gender DESC
